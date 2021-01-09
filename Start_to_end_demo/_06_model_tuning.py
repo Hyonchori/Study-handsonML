955db630aefda4de2c7a03869955222b4d578bbe
Hyonchori <gus8cjf@naver.com>
2021-01-09 17:04:28 +0900

    Update _06_model_tuning.py

--- Start_to_end_demo/_06_model_tuning.py
+++ Start_to_end_demo/_06_model_tuning.py
@@ -18,3 +18,11 @@ grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
 grid_search.fit(housing_prepared, housing_labels)
 
 best_model = grid_search.best_estimator_
+
+
+from _04_preprocessing import num_attribs
+
+extra_attribs = ["rooms_per_house", "pop_per_house", "bedrooms_per_room"]
+cat_encoder = full_pipeline.named_transformers_["tf_cat"]
+cat_one_hot_attribs = list(cat_encoder.categories_[0])
+attribs = num_attribs + extra_attribs + cat_one_hot_attribs
